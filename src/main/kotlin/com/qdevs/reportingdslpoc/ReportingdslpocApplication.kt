eebb3bddc77575587c9a737aef700349ba0c814d
Carlos Quiroga <cquiro@gmail.com>
2021-08-06 12:21:29 +0200

    First commit

---
+++ src/main/kotlin/com/qdevs/reportingdslpoc/ReportingdslpocApplication.kt
@@ -0,0 +1,11 @@
+package com.qdevs.reportingdslpoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportingdslpocApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReportingdslpocApplication>(*args)
+}
---
+++ src/test/kotlin/com/qdevs/reportingdslpoc/ReportingdslpocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.qdevs.reportingdslpoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReportingdslpocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
